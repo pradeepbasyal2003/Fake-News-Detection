42d74f0a832f94eb367492cf8f4dec459d509b33
pradeepbasyal2003 <pradeepbasyal2003@gmail.com>
2025-11-06 22:30:55 +0545

    Recommended articles/news fetch api implemented

--- a_core/settings.py
+++ a_core/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
+SECRET_KEY = os.getenv("SECRET_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-37g45in_vrk_vg=-sa*%jug6)9wd50k0i=96#7t6rx=f9g_lt$'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- a_detection/views.py
+++ a_detection/views.py
@@ -14,36 +14,13 @@ from nltk.corpus import stopwords
 import string
 from .utils import preprocess_text
 from .model_loader import predict
+from a_core.settings import NEWS_API_KEY
+import httpx
+import spacy
+from collections import Counter
 # Custom stopwords for more relevant keywords
-# CUSTOM_STOPWORDS = set([
-#     'said', 'will', 'one', 'two', 'new', 'also', 'can', 'may', 'like', 'just', 'get', 'make', 'time', 'year', 'years', 'day', 'days', 'week', 'weeks', 'month', 'months',
-#     'us', 'mr', 'mrs', 'ms', 'could', 'would', 'should', 'must', 'might', 'still', 'even', 'many', 'much', 'every', 'news', 'report', 'reports', 'say', 'says', 'see', 
-#     'seen', 'use', 'used', 'using', 'however', 'according', 'including', 'since', 'among', 'within', 'without', 'around', 'across', 'per', 'via', 'due', 'yet', 'another', 
-#     'others', 'back', 'ago', 'first', 'last', 'next', 'over', 'under', 'before', 'after', 'between', 'through', 'about', 'against', 'above', 'below', 'off', 'on', 'in', 
-#     'at', 'by', 'to', 'from', 'with', 'for', 'of', 'and', 'or', 'but', 'if', 'as', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'it', 'its', 'this', 'that', 'these',
-#     'those', 'he', 'she', 'they', 'them', 'his', 'her', 'their', 'our', 'your', 'my', 'me', 'you', 'we', 'do', 'does', 'did', 'so', 'no', 'not', 'than', 'then', 'there',
-#     'here', 'out', 'up', 'down', 'into', 'over', 'again', 'once', 'because', 'while', 'where', 'when', 'who', 'whom', 'which', 'what', 'how', 'why', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'only', 'own', 'same', 'too', 'very', 's', 't', 'can', 'will', 'don', 'should', 'now'
-# ])
 
-# Load models and vectorizer once
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-# MODEL_PATH = os.path.join(BASE_DIR, 'model.joblib')
-# VECTORIZER_PATH = os.path.join(BASE_DIR, 'vectorizer.joblib')
-# WORD2VEC_PATH = os.path.join(BASE_DIR, 'word2vec.model')
-# EXPLAINER_MODEL_PATH = os.path.join(BASE_DIR, 'explainer_model.joblib')
 
-# model = joblib.load(MODEL_PATH)
-# vectorizer = joblib.load(VECTORIZER_PATH)
-# word2vec_model = gensim.models.Word2Vec.load(WORD2VEC_PATH)
-# explainer_model = joblib.load(EXPLAINER_MODEL_PATH)
-
-# def document_vector(doc, model):
-#     """Create a document vector by averaging word vectors."""
-#     processed_tokens = preprocess_text(doc)
-#     doc_vectors = [model.wv[word] for word in processed_tokens if word in model.wv.key_to_index]
-#     if not doc_vectors:
-#         return np.zeros(model.vector_size)
-#     return np.mean(doc_vectors, axis=0)
 
 def home_view(request):
     return render(request,'home.html')
@@ -59,7 +36,7 @@ def predict_fake_news(request):
             data = json.loads(request.body)
             title = data.get('title', '')
             body = data.get('body', '')
-
+            print(f"title:{title}")
             result = predict(title, body)
             print(result)
             
@@ -69,56 +46,10 @@ def predict_fake_news(request):
                 result = "REAL" if result["prediction"] == "REAL" else "FAKE",
                 confidence_score = result["confidence"],
             )
+
+            recommended_articles = get_real_news_recommendations(title)
+            result["article recommendations"] = recommended_articles
             return JsonResponse(result)
-        #     # --- Prediction using Word2Vec model ---
-        #     content = title + ' ' + body
-        #     content_vec = document_vector(content, word2vec_model).reshape(1, -1)
-        #     pred = model.predict(content_vec)[0]
-        #     proba = model.predict_proba(content_vec)[0]
-        #     confidence = float(np.max(proba))
-            
-        #     result = {
-        #         'prediction': 'real' if pred == 1 else 'fake',
-        #         'confidence': confidence
-        #     }
-            
-        #     # --- Cosine Similarity using Word2Vec ---
-        #     if title and body:
-        #         title_vec = document_vector(title, word2vec_model).reshape(1, -1)
-        #         text_vec = document_vector(body, word2vec_model).reshape(1, -1)
-        #         similarity_score = cosine_similarity(title_vec, text_vec)[0][0]
-        #         result['similarity'] = f"{similarity_score:.2f} ({similarity_score*100:.0f}% confident match)"
-
-
-        #     # --- Suspicious words using TF-IDF Explainer Model ---
-        #     # This is the correct way to find words that influenced the decision.
-        #     X_tfidf = vectorizer.transform([content])
-        #     feature_names = np.array(vectorizer.get_feature_names_out())
-        #     explainer_coefs = explainer_model.coef_[0]
-
-        #     # Get indices of words from the input text that are in our vocabulary
-        #     input_indices = X_tfidf.nonzero()[1]
-
-        #     # Get the words and their corresponding coefficients from the explainer model
-        #     input_words = feature_names[input_indices]
-        #     input_coefs = explainer_coefs[input_indices]
-            
-        #     # Pair words with their coefficients
-        #     word_coef_pairs = list(zip(input_words, input_coefs))
-
-        #     if pred == 0:  # Fake news is class 0. High positive coefficients indicate "fake".
-        #         word_coef_pairs.sort(key=lambda x: -x[1])
-        #     else:  # Real news is class 1. High negative coefficients indicate "real".
-        #         word_coef_pairs.sort(key=lambda x: x[1])
-            
-        #     keywords = [word for word, coef in word_coef_pairs[:10]]
-            
-        #     if len(keywords) > 0:
-        #         result['keywords'] = keywords
-                
-        #     return JsonResponse(result)
-        # except Exception as e:
-        #     return JsonResponse({'error': str(e)}, status=400)
     else:
         return redirect("/")
 
@@ -126,8 +57,6 @@ def predict_fake_news(request):
 
 
 def submit_feedback(request):
-    
-
     if request.method == 'POST':
         form = UserFeedbackForm(request.POST)
         if form.is_valid():
@@ -141,3 +70,42 @@ def submit_feedback(request):
 
     return render(request, 'feedback_form.html', {'form': form})
 
+
+
+def get_real_news_recommendations(title : str):
+    """
+    Returns a list of real news articles that are similar to the given title.
+    """
+    query = get_boarder_query(title)
+
+    url = f"https://newsapi.org/v2/everything?q={query}&sortBy=relevancy&pageSize=5&apiKey={NEWS_API_KEY}"
+
+    client = httpx.Client()
+    response = client.get(url)
+    print(f"response :: {response.text}")
+    data = response.json()
+
+    articles = []
+    for a in data.get("articles", []):
+        articles.append({
+            "title": a["title"],
+            "source": a["source"]["name"],
+            "url": a["url"],
+            "description": a["description"]
+        })
+    print(articles)
+    return articles
+
+
+def get_boarder_query(title:str):
+    """
+    Returns a boarder query after filtering unnecessary words.
+    """
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(title)
+
+    # Extract nouns and proper nouns as keywords
+    keywords = [token.text for token in doc if token.pos_ in ["NOUN", "PROPN"]]
+    broad_query = " ".join(keywords)
+    print(broad_query)
+    return broad_query
